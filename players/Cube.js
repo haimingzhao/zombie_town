4238d5ffac802dc1efdc3f7b61613aaa625bc956
Joanna <jd3012@ic.ac.uk>
2014-06-08 21:17:01 +0100

    cleaned up the code

--- players/Cube.js
+++ players/Cube.js
@@ -9,44 +9,38 @@ var scene,
 var socket = io.connect();
 
 function init () {
-			// socket.on('connected', function() {
+	// socket.on('connected', function() {
 
-			scene = new THREE.Scene();
-			camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
-			renderer = new THREE.WebGLRenderer();
-			renderer.setSize(window.innerWidth, window.innerHeight);
-			document.body.appendChild(renderer.domElement);
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
 
-			var geometry = new THREE.CubeGeometry(1,1,1);
-			var material = new THREE.MeshBasicMaterial({color: 0x458B00});
-			zombie = new THREE.Mesh(geometry, material);
-			scene.add(zombie);
+	var geometry = new THREE.CubeGeometry(1,1,1);
+	var material = new THREE.MeshBasicMaterial({color: 0x458B00});
+	zombie = new THREE.Mesh(geometry, material);
+	scene.add(zombie);
 
-			var geometry = new THREE.CubeGeometry(1,1,1);
-			var material = new THREE.MeshBasicMaterial({color: 0xffb4d9});
-			slayer = new THREE.Mesh(geometry, material);
-			slayer.position.x = 3;
-			slayer.position.y = 1;
-			scene.add(slayer);
+	var geometry = new THREE.CubeGeometry(1,1,1);
+	var material = new THREE.MeshBasicMaterial({color: 0xffb4d9});
+	slayer = new THREE.Mesh(geometry, material);
+	slayer.position.x = 3;
+	slayer.position.y = 1;
+	scene.add(slayer);
 
-			// var geometry = new THREE.CubeGeometry(1,1,1);
-			// var material = new THREE.MeshBasicMaterial({color: 0xffb4d9});
-			// var slayer = new THREE.Mesh(geometry, material);
-			// scene.add(slayer);
+	camera.position.z = 5;
 
-			camera.position.z = 5;
+	var render = function () {
+		requestAnimationFrame(render);
+		renderer.render(scene, camera);
 
-			var render = function () {
-				requestAnimationFrame(render);
+		movement();
 
-				renderer.render(scene, camera);
+	}
 
-				movement();
-
-			}
-
-			render();
+	render();
 };
 
 $(document).ready(function() {
@@ -78,81 +72,31 @@ $(document).ready(function() {
 			var newx;
 			var newy; 
 
-			// if('cur_room' in obj) {
-			// 	console.log(obj.cur_room);
-			// 	if(obj.cur_room === room) {
-
 			if ('zombie' in obj.newPosition) {
 				zombie.position.x = obj.newPosition.zombie[0].x;
 				zombie.position.y = obj.newPosition.zombie[0].y;
-				// newx = obj.newPosition.zombie[0].x;
-				// newy = obj.newPosition.zombie[0].y; 
-				// zombie.position.x = newx;
-				// zombie.position.y = newy; 
 			} else {
 				slayer.position.x = obj.newPosition.slayer[0].x;
 				slayer.position.y = obj.newPosition.slayer[0].y;
-				// newx = obj.newPosition.slayer[0].x;
-				// newy = obj.newPosition.slayer[0].y; 
-				// slayer.position.x = newx;
-				// slayer.position.y = newy;
 			}
-
-
-			// 	}
-			// }
-
-			// if ('zombie' in obj.newPosition) {
-			// 	zombie.position.x = obj.newPosition.zombie[0].x;
-			// 	zombie.position.y = obj.newPosition.zombie[0].y;
-			// 	// newx = obj.newPosition.zombie[0].x;
-			// 	// newy = obj.newPosition.zombie[0].y; 
-			// 	// zombie.position.x = newx;
-			// 	// zombie.position.y = newy; 
-			// } else {
-			// 	slayer.position.x = obj.newPosition.slayer[0].x;
-			// 	slayer.position.y = obj.newPosition.slayer[0].y;
-			// 	// newx = obj.newPosition.slayer[0].x;
-			// 	// newy = obj.newPosition.slayer[0].y; 
-			// 	// slayer.position.x = newx;
-			// 	// slayer.position.y = newy;
-			// }
-
-
-			// var newz...
-
-			// if (clientid === 0) /* we are the zombie */ {
-			// 	slayer.position.x = newx; 
-			// 	slayer.position.y = newy; 
-			// 	console.log('We have just updated the slayer position.\n'); 
-			// } else { /* we are the slayer */
-			// 	zombie.position.x = newx;
-			// 	zombie.position.y = newy; 
-			// 	console.log('We have just updated the zombie position.\n'); 
-			// }
 		}
-		// if('A_Zombie' in message) {
-		// 	// console.log('returned ' + message);
-		// }
 		if('id' in obj) {
 			console.log('in id with ' + JSON.stringify(obj.id));
 			// Setting the client id. ID == 0 -> zombie. ID == 1 -> slayer. 
 			clientid = obj.id; 
 			console.log('after assignment ' + JSON.stringify(clientid));
-
-			// this.clientid = obj.id[0];
 		}
 	});
 });
 
 	function movement() {
+		// Zombie player control
 		if (clientid%2 === 0) {  	
 		      if(Key.isDown(Key.A)) {
 		      	console.log('client id = 0');
-		      	zombie.position.x -= 0.1;
-		      	// camera.position.x -= 0.1;
-		      	socket.send({'zombie': [zombie.position]});
-		      	console.log(zombie.position);
+			    zombie.position.x -= 0.1;
+			    // camera.position.x -= 0.1;
+			    socket.send({'zombie': [zombie.position]});
 		      }
 		      if(Key.isDown(Key.W)) {
 		        zombie.position.y += 0.1;
@@ -169,12 +113,10 @@ $(document).ready(function() {
 		        // camera.position.x += 0.1;
 		        socket.send({'zombie': [zombie.position]});
 		      }
-
 		} else if(clientid%2 === 1) {
-		     	
-		     	
+		     // Slayer player control
 		     if(Key.isDown(Key.A)) {
-		     		console.log('client id = 1');
+		     	console.log('client id = 1');
 		     	slayer.position.x -= 0.1;
 		      	// camera.position.x -= 0.1;
 		      	socket.send({'slayer': [slayer.position]});
@@ -195,6 +137,5 @@ $(document).ready(function() {
 		        // camera.position.x += 0.1;
 		        socket.send({'slayer': [slayer.position]});
 		      }
-
 		}
 };
--- players/server.js
+++ players/server.js
@@ -5,9 +5,8 @@ var http = require('http'),
     THREE = require('three');
 
 var server,
-    players = [],
-    rooms = ['room1'];
-
+    players = [];
+    
 server = http.createServer(function(req, res){
     var path = url.parse(req.url).pathname;
     // console.log(path);
@@ -18,14 +17,12 @@ server = http.createServer(function(req, res){
         case '/':
             if (path == '/') path = '/client.html';
             fs.readFile(__dirname + path, function(err, data) {
-            if (err) return send404(res, "::err:" + path);
-            // res.writeHead(200, {'Content-Type': 'text/html'})
-
-            res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'});
-            res.write(data, 'utf8');
-            res.end();
+                if (err) return send404(res, "::err:" + path);
+                res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'});
+                res.write(data, 'utf8');
+                res.end();
             });
-        break;
+            break;
         default:
         send404(res, "not found: " + path);
     break;
@@ -51,46 +48,17 @@ socket.on('connection', function(client){
     client.send({id: client.id});
     console.log({id: client.id});
 
-    // Create each room for 2 players
-    // if(client.id%2 === 0) {
-    //     //rooms.push(rooms.length);
-    //     client.room = 'room1';
-    //     client.join('room1');
-    // } else if(client.id%2 === 1) {
-    //     client.room = 'room1';
-    //     client.join('room1');
-    // }
     client.room = 'room' + (Math.floor(client.id/2)).toString();
 
-    // console.log(rooms.length - 1);
     client.send({room: client.room});
 
     client.on('message',function(message) {
         if('zombie' in message) {
             console.log('zombie in room ' + client.room + JSON.stringify(message));
-            // if(client.id%2 === 0) {
-                players[client.id+1].send({'newPosition': message});
-            // }
-            // } else {
-            //     // if(client.id%2 === 1) {
-            //     players[cilent.id-1].send({'newPosition': message});
-            // // }
-            // }
-            // var i;
-            // for(i = 0; i < players.length; i++) {
-            //     console.log(players[i] + 'room '  + players[i].room);
-            //     players[i].in('room1').send({'newPosition': message});
-            //     // players[i].send({'newPosition': message, 'cur_room': client.room});
-            // }
+            players[client.id+1].send({'newPosition': message});
         } else if('slayer' in message){
             console.log('slayer in room ' + client.room + JSON.stringify(message));
             players[client.id-1].send({'newPosition': message});
-            // var i;
-            // for(i = 0; i < players.length; i++) {
-            //     console.log(players[i] +  'room ' + players[i].room);
-            //     players[i].in('room1').send({'newPosition': message});
-            //     players[i].send({'newPosition': message, 'cur_room': client.room});
-            // }
         }
     });
     
