c092d8d177b294dc35292c266eac31b13c286572
Joanna <jd3012@ic.ac.uk>
2014-06-09 15:28:28 +0100

    player boundries implementing

--- players/Cube.js
+++ players/Cube.js
@@ -9,8 +9,6 @@ var scene,
 var socket = io.connect();
 
 function init () {
-	// socket.on('connected', function() {
-
 	scene = new THREE.Scene();
 	camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
@@ -58,84 +56,113 @@ $(document).ready(function() {
 			console.log('we are in room ' + obj.room);
 			room = obj.room;
 		}
-		// console.log('***' + JSON.stringify(obj));
 		if('newPosition' in obj) {
-			// console.log('newPosition ' + JSON.stringify(obj));
-			// console.log('x =' + JSON.stringify(obj.newPosition.zombie[0].x));
-
 			if (clientid === 0) {
 				console.log('We are the zombie.\n'); 
 			} else {
 				console.log('We are the slayer.\n'); 
 			}
 
-			var newx;
-			var newy; 
-
 			if ('zombie' in obj.newPosition) {
 				zombie.position.x = obj.newPosition.zombie[0].x;
 				zombie.position.y = obj.newPosition.zombie[0].y;
+				if(inRange()) {
+					console.log('zombie turned slayer');
+				}
+
 			} else {
 				slayer.position.x = obj.newPosition.slayer[0].x;
 				slayer.position.y = obj.newPosition.slayer[0].y;
+				if(inRange()) {
+					console.log('slayer killed zombie');
+				}
 			}
 		}
 		if('id' in obj) {
 			console.log('in id with ' + JSON.stringify(obj.id));
-			// Setting the client id. ID == 0 -> zombie. ID == 1 -> slayer. 
 			clientid = obj.id; 
 			console.log('after assignment ' + JSON.stringify(clientid));
 		}
 	});
 });
 
-	function movement() {
-		// Zombie player control
-		if (clientid%2 === 0) {  	
-		      if(Key.isDown(Key.A)) {
-		      	console.log('client id = 0');
-			    zombie.position.x -= 0.1;
-			    // camera.position.x -= 0.1;
-			    socket.send({'zombie': [zombie.position]});
-		      }
-		      if(Key.isDown(Key.W)) {
-		        zombie.position.y += 0.1;
-		        // camera.position.y += 0.1;
-		        socket.send({'zombie': [zombie.position]});
-		      }
-		      if(Key.isDown(Key.S)) {
-		        zombie.position.y -= 0.1;
-		        // camera.position.y -= 0.1;
-		        socket.send({'zombie': [zombie.position]});
-		      }
-		      if(Key.isDown(Key.D)) {
-		        zombie.position.x += 0.1;
-		        // camera.position.x += 0.1;
-		        socket.send({'zombie': [zombie.position]});
-		      }
-		} else if(clientid%2 === 1) {
-		     // Slayer player control
-		     if(Key.isDown(Key.A)) {
-		     	console.log('client id = 1');
-		     	slayer.position.x -= 0.1;
-		      	// camera.position.x -= 0.1;
-		      	socket.send({'slayer': [slayer.position]});
-		      	console.log(slayer.position);
-		      }
-		      if(Key.isDown(Key.W)) {
-		        slayer.position.y += 0.1;
-		        // camera.position.y += 0.1;
-		        socket.send({'slayer': [slayer.position]});
-		      }
-		      if(Key.isDown(Key.S)) {
-		        slayer.position.y -= 0.1;
-		        // camera.position.y -= 0.1;
-		        socket.send({'slayer': [slayer.position]});
-		      }
-		      if(Key.isDown(Key.D)) {
-		        slayer.position.x += 0.1;
-		        // camera.position.x += 0.1;
-		        socket.send({'slayer': [slayer.position]});
-		      }
+function movement() {
+	if (clientid%2 === 0) {  	
+        if(Key.isDown(Key.A)) {
+	      	console.log('client id = 0');
+	      	zombie.position.x -= 0.1;
+	      	// camera.position.x -= 0.1;
+	      	socket.send({'zombie': [zombie.position]});
+	      	console.log(zombie.position);
+        }
+        if(Key.isDown(Key.W)) {
+	        zombie.position.y += 0.1;
+	        // camera.position.y += 0.1;
+	        socket.send({'zombie': [zombie.position]});
+        }
+        if(Key.isDown(Key.S)) {
+	        zombie.position.y -= 0.1;
+	        // camera.position.y -= 0.1;
+	        socket.send({'zombie': [zombie.position]});
+        }
+        if(Key.isDown(Key.D)) {
+	        zombie.position.x += 0.1;
+	        // camera.position.x += 0.1;
+	        socket.send({'zombie': [zombie.position]});
+        }
+
+	} else if(clientid%2 === 1) {     	
+	    if(Key.isDown(Key.A)) {
+			console.log('client id = 1');
+			slayer.position.x -= 0.1;
+			// camera.position.x -= 0.1;
+			socket.send({'slayer': [slayer.position]});
+			console.log(slayer.position);
 		}
+		if(Key.isDown(Key.W)) {
+		    slayer.position.y += 0.1;
+		    // camera.position.y += 0.1;
+		    socket.send({'slayer': [slayer.position]});
+		    }
+		if(Key.isDown(Key.S)) {
+		    slayer.position.y -= 0.1;
+		    // camera.position.y -= 0.1;
+		    socket.send({'slayer': [slayer.position]});
+		}
+		if(Key.isDown(Key.D)) {
+		    slayer.position.x += 0.1;
+		    // camera.position.x += 0.1;
+		    socket.send({'slayer': [slayer.position]});
+		}
+	}
+};
+
+function inRange() {
+	var isInRange = false,
+		slayerPosX = slayer.position.x,
+		slayerPosY = slayer.position.y,
+		zombiePosX = zombie.position.x,
+		zombiePosY = zombie.position.y;
+		console.log('slayer: ' + slayerPosX + ' ' + slayerPosY);
+		console.log('zombie: ' + zombiePosX + ' ' + zombiePosY);
+	if(clientid === 0) {
+		if(
+			((zombiePosX < slayerPosX + 1) && (zombiePosY < slayerPosY + 1)) 
+			&& ((zombiePosX < slayerPosX + 1) && (zombiePosY > slayerPosY - 1))
+			&& ((zombiePosX > slayerPosX - 1) && (zombiePosY > slayerPosY - 1)) 
+			&& ((zombiePosX > slayerPosX - 1) && (zombiePosY < slayerPosY + 1))
+			) {
+			isInRange = true;
+		}
+	} else if(clientid === 1) {
+		if((
+			(slayerPosX < zombiePosX + 1) && (slayerPosY < zombiePosY + 1)) 
+			&& ((slayerPosX < zombiePosX + 1) && (slayerPosY > zombiePosY - 1))
+			&& ((slayerPosX > zombiePosX - 1) && (slayerPosY > zombiePosY - 1)) 
+			&& ((slayerPosX > zombiePosX - 1) && (slayerPosY < zombiePosY + 1))
+			) {
+			isInRange = true;
+		}
+	}
+	return isInRange;
 };
--- players/server.js
+++ players/server.js
@@ -6,10 +6,9 @@ var http = require('http'),
 
 var server,
     players = [];
-    
+
 server = http.createServer(function(req, res){
     var path = url.parse(req.url).pathname;
-    // console.log(path);
     switch (path) {
         case '/lib/three.js':
         case '/lib/keyboard.js':
@@ -17,12 +16,14 @@ server = http.createServer(function(req, res){
         case '/':
             if (path == '/') path = '/client.html';
             fs.readFile(__dirname + path, function(err, data) {
-                if (err) return send404(res, "::err:" + path);
-                res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'});
-                res.write(data, 'utf8');
-                res.end();
+            if (err) return send404(res, "::err:" + path);
+            // res.writeHead(200, {'Content-Type': 'text/html'})
+
+            res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'});
+            res.write(data, 'utf8');
+            res.end();
             });
-            break;
+        break;
         default:
         send404(res, "not found: " + path);
     break;
@@ -49,11 +50,10 @@ socket.on('connection', function(client){
     console.log({id: client.id});
 
     client.room = 'room' + (Math.floor(client.id/2)).toString();
-
     client.send({room: client.room});
 
     client.on('message',function(message) {
-        if('zombie' in message) {
+        if('zombie' in message && players[client.id+1] !== null) {
             console.log('zombie in room ' + client.room + JSON.stringify(message));
             players[client.id+1].send({'newPosition': message});
         } else if('slayer' in message){
@@ -61,10 +61,17 @@ socket.on('connection', function(client){
             players[client.id-1].send({'newPosition': message});
         }
     });
+
+    client.on('zombieWin', function(message) {
+        console.log('zombie wins!!');
+    });
+    client.on('slayerWin', function(message) {
+        console.log('slayer wins!!');
+    });
     
     client.on('disconnect', function() {
         players.splice(client.sessionId,1);
-        return client.emit({ disconnect: [client.sessionId]})
         console.log('disconnect');
+        return client.emit({ disconnect: [client.sessionId]})
     });
 });
